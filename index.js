9fef00a679aa09c98c07ca8cdfa7be1dc6553a07
Tylor Sherman <tylorsherman@gmail.com>
2012-02-21 11:24:56 -0800

    Try hello world...

--- index.js
+++ index.js
@@ -1,46 +1,10 @@
 var express = require('express');
-var expressHogan = require('express-hogan.js');
-var app = module.exports = express.createServer();
 
-app.register('.html', expressHogan);
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+var app = express.createServer(express.logger());
 
-app.configure('development', function(){
-  app.use(express.static(__dirname + '/public', { maxAge: 31557600000 })); /* One year */
-  app.use(express.bodyParser());
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
-
-app.get('/', function(req, res){
-  res.render('index');
-});
-
-app.get('/sighting', function(req, res) {
-  returnSighting(req, res);
-});
-
-app.post('/sighting', function(req, res) {
-  // Take care of post, redirect to get.
-  returnSighting(req, res);
-});
-
-var returnSighting = function(req, res) {
-  var location = null;
-  var total = 0;
-
-  for (var key in req.body.beard) {
-    total += parseInt(req.body.beard[key]);
-  }
-  if (req.body.location && req.body.location.latitude && req.body.location.longitude) {
-    location = req.body.location;
-   }
-  res.render('sighting', {
-    locals: {
-      total: total,
-      location: location
-    }
-  });
-}
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
