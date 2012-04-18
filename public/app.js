4fef43b7c8e4f4e213e83fcf8fc5b810fa02907f
Tylor Sherman <tylorsherman@gmail.com>
2012-04-17 17:10:28 -0700

    Add cookie for nickname

--- public/app.js
+++ public/app.js
@@ -1,4 +1,16 @@
 /**
+ * jQuery Cookie plugin
+ *
+ * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * https://github.com/carhartl/jquery-cookie
+ */
+(function(a){a.cookie=function(b,c,d){if(arguments.length>1&&(!/Object/.test(Object.prototype.toString.call(c))||c===null||c===undefined)){d=a.extend({},d);if(c===null||c===undefined){d.expires=-1}if(typeof d.expires==="number"){var e=d.expires,f=d.expires=new Date;f.setDate(f.getDate()+e)}c=String(c);return document.cookie=[encodeURIComponent(b),"=",d.raw?c:encodeURIComponent(c),d.expires?"; expires="+d.expires.toUTCString():"",d.path?"; path="+d.path:"",d.domain?"; domain="+d.domain:"",d.secure?"; secure":""].join("")}d=c||{};var g=d.raw?function(a){return a}:decodeURIComponent;var h=document.cookie.split("; ");for(var i=0,j;j=h[i]&&h[i].split("=");i++){if(g(j[0])===b)return g(j[1]||"")}return null}})(jQuery);
+
+/**
 *
 * NoClickDelay
 * http://cubiq.org/
@@ -70,6 +82,11 @@ $(document).ready(function() {
     return false;
   });
 
+  var nickname = $.cookie('nickname') || null;
+  if (nickname !== null) {
+    $('#nickname').val(nickname);
+  }
+
   $('#submit-beards').click(function() {
     if($('#nickname').val() == '') {
       $('#beards').hide();
@@ -77,6 +94,10 @@ $(document).ready(function() {
       $('#nickname').focus();
       return false;
     }
+  });
+  $('#submit-name').click(function() {
+    $.cookie('nickname', $('#nickname').val(), { expires: 90, path: '/' });
+    return true;
   });
 });
 
